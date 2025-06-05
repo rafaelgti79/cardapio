4410eed8c26e22d0c6559da8e8ca985be4ff0602
rafaelgti79 <rafaelgti79@gmail.com>
2025-06-05 15:20:18 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='container'>
+      <div>
+        <div className='titulo'>
+        <h1>TESTE PARA PAGINA</h1>
+        <div className="linha-customizada"></div>
+        </div>
+
+      <div className='containerSub'>
+       <h1 className='tituloCard'>CARNES</h1>
+       <h2 className='lista'>CONTRA FILE A BRASILEIRA ............................ R$ 30,00</h2>
+       <h2 className='lista'>CONTRA FILE A CAVALO ................................... R$ 30,00</h2>
+       <h2 className='lista'>CONTRA FILE COM LEGUMES .......................... R$ 30,00</h2>
+       <h2 className='lista'>CHURRASCO MISTO .................................................... R$ 30,00</h2>
+       <h2 className='lista'>CONTRA FÍLE COM ARROZ E BROCOLIS ............... R$ 30,00</h2>
+      </div>
+
+      <div className='containerSub'>
+       <h1 className='tituloCard'>FRANGO</h1>
+       <h2 className='lista'>FRNAGO GRELHADO ..................................... R$ 30,00</h2>
+       <h2 className='lista'>FRANGO A PARMEGIANA ................................. R$ 30,00</h2>
+       <h2 className='lista'>FRANGO A MILANESA ................................... R$ 30,00</h2>
+       <h2 className='lista'>FRANGO GRELHADO COM LEGUMES ......................... R$ 30,00</h2>
+       <h2 className='lista'>perna de ganço ...................................... R$ 30,00</h2>
+      </div>
+
+      <div className='containerSub'>
+       <h1 className='tituloCard'>CARNE SUINA</h1>
+       <h2 className='lista'>CARRE A MINEIRA ..................................... R$ 30,00</h2>
+       <h2 className='lista'>CARRE FRITO ......................................... R$ 30,00</h2>
+       <h2 className='lista'>VIRADINHO A PAULISTA  ............................... R$ 30,00</h2>
+       
+      </div>
+
+        
+      
+    </div>
+  </div>
+  )
+}
+
+export default App
+     
+      
